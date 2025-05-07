868fbd27d5de076f43b5ab177fe7669e338fad5a
hyerijung <hyerijung0903@gmail.com>
2025-05-08 00:24:39 +0900

    add op tag for link thumbnail

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
 import Header from "components/Header";
 import Features from "components/Features";
-import Section from "components/Section";
 import Footer from "components/Footer";
-import Image from "next/image";
 import Details from "components/Details";
-import Download from "components/Download";
 import Main from "components/Main";
+import Head from 'next/head';
+
 
 export default function Page() {
   return (
+    <>
+     <Head>
+        <title>Mobile Invitation</title>
+        {/* open graph */}
+        <meta name="description" content="Mobile Invitation - description" />
+        <meta property="og:title" content="Mobile Invitation"/>
+        <meta property="og:description" content="Mobile Invitation - description"/>
+        <meta property="og:image" content="https://mobile-invitation-rouge.vercel.app/invitation.png" />
+        <meta property="og:url" content="https://mobile-invitation-rouge.vercel.app/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:site_name" content="Mobile Invitation Template"/>
+        {/* twitter card */}
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:title" content="Mobile Invitation"/>
+        <meta name="twitter:description" content="Mobile Invitation - description"/>
+        <meta name="twitter:image" content="https://mobile-invitation-rouge.vercel.app/invitation.png"/>
+    </Head>
     <div className="flex flex-col min-h-screen bg-white dark:bg-black">
       <Header />
       <main>
@@ -19,5 +35,6 @@ export default function Page() {
       </main>
       <Footer />
     </div>
+    </>
   );
 }
